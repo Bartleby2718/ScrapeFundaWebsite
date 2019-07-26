4fe1eece5bc63518889d7f953b0165f3c45747a5
Bartleby <bartleby2718@gmail.com>
2019-07-26 17:43:57 +0900

    Refactor code to prepare for multiprocessing close #10

--- scrape_funda.py
+++ scrape_funda.py
@@ -1,13 +1,18 @@
+from typing import List, Tuple
 import bs4
 import datetime
 import requests
 import xlsxwriter
 
+XLSX_FILE_NAME: str = 'Funda.xlsx'
+
 
 def write_row_in_xlsx(workbook, worksheet, row_num: int, index: int, borrower: str, annual_rate: float, duration: int,
                       partially_covered: bool, limit: int, credit_rating: int, info: str):
-    intro_format = workbook.add_format({'align': 'left', 'text_wrap': True, 'valign': 'top'})
-    non_intro_format = workbook.add_format({'align': 'center', 'valign': 'vcenter'})
+    intro_format_dict: dict = {'align': 'left', 'text_wrap': True, 'valign': 'top'}
+    intro_format: xlsxwriter.workbook.Format = workbook.add_format(intro_format_dict)
+    non_intro_format_dict: dict = {'align': 'center', 'valign': 'vcenter'}
+    non_intro_format: xlsxwriter.workbook.Format = workbook.add_format(non_intro_format_dict)
 
     worksheet.write_number(row_num, 0, index, non_intro_format)
     worksheet.write(row_num, 1, borrower, non_intro_format)
@@ -19,56 +24,36 @@ def write_row_in_xlsx(workbook, worksheet, row_num: int, index: int, borrower: s
     worksheet.write(row_num, 7, info, intro_format)
 
 
-XLSX_FILE_NAME: str = 'Funda.xlsx'
-list_url: str = 'https://www.funda.kr/v2/invest/list'
-list_response: requests.Response = requests.get(list_url)
-list_html_soup: bs4.element.ResultSet = bs4.BeautifulSoup(list_response.text, 'html.parser')
-products: bs4.element.Tag = list_html_soup.find('div', id='general_merchandise')
-product_titles: bs4.element.ResultSet = products.find_all('span', class_='merchandise-idx')
+def create_custom_workbook() -> Tuple[xlsxwriter.workbook.Workbook, xlsxwriter.workbook.Worksheet]:
+    # Create Excel workbook
+    workbook: xlsxwriter.workbook.Workbook = xlsxwriter.Workbook(XLSX_FILE_NAME)
+    now: str = datetime.datetime.today().strftime('%Y%m%d %H%M%S')
+    worksheet: xlsxwriter.workbook.Worksheet = workbook.add_worksheet(now)
 
-# Create Excel workbook
-workbook = xlsxwriter.Workbook(XLSX_FILE_NAME)
-now: str = datetime.datetime.today().strftime('%Y%m%d %H%M%S')
-worksheet = workbook.add_worksheet(now)
+    # Write headers in xlsx
+    header_format_dict: dict = {'align': 'center', 'bold': True, 'valign': 'vcenter'}
+    header_format: xlsxwriter.workbook.Format = workbook.add_format(header_format_dict)
+    worksheet.write(0, 0, 'Index', header_format)
+    worksheet.write(0, 1, 'Borrower', header_format)
+    worksheet.write(0, 2, 'Annual Rate (%)', header_format)
+    worksheet.write(0, 3, 'Duration (month)', header_format)
+    worksheet.write(0, 4, 'Safe Plan', header_format)
+    worksheet.write(0, 5, 'Limit', header_format)
+    worksheet.write(0, 6, 'Credit Rating', header_format)
+    worksheet.set_column('A:G', 15)
+    worksheet.set_column('B:B', 25)
+    worksheet.write(0, 7, 'Info', header_format)
+    worksheet.set_column('H:H', 65)
+    return workbook, worksheet
 
-# Write headers in xlsx
-header_format = workbook.add_format({'align': 'center', 'bold': True, 'valign': 'vcenter'})
-worksheet.write(0, 0, 'Index', header_format)
-worksheet.write(0, 1, 'Borrower', header_format)
-worksheet.write(0, 2, 'Annual Rate (%)', header_format)
-worksheet.write(0, 3, 'Duration (month)', header_format)
-worksheet.write(0, 4, 'Safe Plan', header_format)
-worksheet.write(0, 5, 'Limit', header_format)
-worksheet.write(0, 6, 'Credit Rating', header_format)
-worksheet.set_column('A:G', 25)
-worksheet.write(0, 7, 'Info', header_format)
-worksheet.set_column('H:H', 65)
 
-title: bs4.element.Tag
-row_num: int = 0
-for title in product_titles:
-    body: bs4.element.Tag = title.parent.parent.parent.next_sibling.next_sibling
-    children: bs4.element.ResultSet = body.find_all('div')
-    # 수익률 (리스트 페이지)
-    annual_rate: str = children[2].dd.text.replace('%', '').strip()
-    # 기간 (리스트 페이지)
-    duration: int = int(children[3].dd.text.replace('개월', ''))
-    # I want only the bonds with the annual rate of 9% and the duration of 3 months
-    if annual_rate != '9' or duration != 3:
-        continue
-
-    row_num += 1
-    # 호수 (리스트 페이지)
-    index: str = title.text.replace('호', '')
-
-    # URL
-    detail_url: str = 'https://www.funda.kr/v2/invest/?page=' + index
+def unsecured_bonds(detail_url: str, workbook, worksheet, row_num: int, index: str, annual_rate: str, duration: int):
     detail_response: requests.Response = requests.get(detail_url)
     detail_html_soup: bs4.element.ResultSet = bs4.BeautifulSoup(detail_response.text, 'html.parser')
 
     # 호수 (디테일 페이지 - 헤더)
     # index_tag: bs4.element.Tag = detail_html_soup.find(id='invest-number')
-    # index: int = int(index_tag.text.replace('호', ''))
+    # index: str = index_tag.text.replace('호', '')
 
     headers: bs4.element.ResultSet = detail_html_soup.find_all('li', class_='de-info-fir-li')
     # 대출자 (디테일 페이지 - 헤더)
@@ -77,11 +62,11 @@ for title in product_titles:
 
     # 기간 (디테일 페이지 - 상단)
     # duration_tag: bs4.element.Tag = headers[0]
-    # duration: str = duration_tag.span.text
+    # duration: int = int(duration_tag.span.text.replace('개월', ''))
 
     # 수익률 (디테일 페이지 - 상단)
     # annual_rate_tag: bs4.element.Tag = headers[1]
-    # annual_rate: str = annual_rate_tag.span.text
+    # annual_rate: str = annual_rate_tag.span.text.replace('%!', '')
 
     # 세이프플랜 (상단)
     safe_plan_tag: bs4.element.Tag = headers[2]
@@ -105,8 +90,7 @@ for title in product_titles:
     # 기존 신용대출정보(디테일 페이지 - 하단)
     history: bs4.element.Tag = detail_html_soup.find(class_='c-loan-history')
     records = {}
-    record: bs4.element.ResultSet
-    for record in history.div.table.tbody.find_all('tr'):
+    for record in history.div.table.tbody.find_all('tr'):  # record: bs4.element.ResultSet
         institution: bs4.element.Tag = record.td
         amount_tag: bs4.element.Tag = institution.next_sibling.next_sibling
         amount: str = amount_tag.span.text
@@ -115,12 +99,48 @@ for title in product_titles:
     # 상점 소개 (디테일 페이지 - 하단)
     intro_statements: bs4.element.ResultSet = detail_html_soup.find_all(class_='list_ok introduce')
     intros = []
-    for block in intro_statements:
-        line: bs4.element.ResultSet
-        for line in block.find_all('li'):
+    for block in intro_statements:  # block: bs4.element.ResultSet
+        for line in block.find_all('li'):  # line: bs4.element.ResultSet
             intros.append(line.text)
     write_row_in_xlsx(workbook, worksheet, row_num, int(index), borrower, float(annual_rate), duration,
                       partially_covered, limit, credit_rating, '\n'.join(intros))
-worksheet.set_default_row(20)  # partially show the second row of store info
-worksheet.set_row(0, 14.4)  # reset header row heightn
-workbook.close()
+
+
+def main():
+    list_url: str = 'https://www.funda.kr/v2/invest/list'
+    list_response: requests.Response = requests.get(list_url)
+    list_html_soup: bs4.element.ResultSet = bs4.BeautifulSoup(list_response.text, 'html.parser')
+    products: bs4.element.Tag = list_html_soup.find('div', id='general_merchandise')
+    product_titles: bs4.element.ResultSet = products.find_all('span', class_='merchandise-idx')
+
+    workbook, worksheet = create_custom_workbook()
+    row_num: int = 0
+    page_info_list: List[tuple] = []
+    for title in product_titles:  # title: bs4.element.Tag
+        body: bs4.element.Tag = title.parent.parent.parent.next_sibling.next_sibling
+        children: bs4.element.ResultSet = body.find_all('div')
+
+        # 수익률 (리스트 페이지)
+        annual_rate: str = children[2].dd.text.replace('%', '').strip()
+        # 기간 (리스트 페이지)
+        duration: int = int(children[3].dd.text.replace('개월', ''))
+        # I want only the bonds with the annual rate of 9% and the duration of 3 months
+        if annual_rate != '9' or duration != 3:
+            continue
+        row_num += 1
+
+        # 호수 (리스트 페이지)
+        index: str = title.text.replace('호', '')
+
+        # URL
+        detail_url: str = 'https://www.funda.kr/v2/invest/?page=' + index
+        page_info: tuple = (detail_url, workbook, worksheet, row_num, index, annual_rate, duration)
+        page_info_list.append(page_info)
+        unsecured_bonds(*page_info)
+    worksheet.set_default_row(20)  # partially show the second row of store info
+    worksheet.set_row(0, 14.4)  # reset header row height
+    workbook.close()
+
+
+if __name__ == '__main__':
+    main()
